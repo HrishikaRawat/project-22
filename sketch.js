1a2581ce50c8fa5d251175aaece518dc0b78b480
HrishikaRawat <75683160+HrishikaRawat@users.noreply.github.com>
2021-01-07 14:25:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground, box1,box2,box3;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1=new Box(100,100,50,50)
+    box2=new Box(120,200,20,20)
+    box3=new Box(200,100,40,30)
+
+    ground=new Ground(200,390,400,10)
+
+
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+
+    box1.display()
+    box2.display()
+    box3.display()
+
+    ground.display()
+
+    
+}
